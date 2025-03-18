e5ec2acf2ef07e7604732dc5f9f29efb2564db12
IntelNuiAu <rusinov307@gmail.com>
2025-03-18 22:43:41 +0300

    new-branch changed readme and main

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-
-print('hello world')
-print('Это изменинение доступно для new-branch')
-print(f'new branch1')
-
-def home():
-    print(f'hello')
-
-home()
-home()
-print(f'lowtab')
-
+print('new-branch')
